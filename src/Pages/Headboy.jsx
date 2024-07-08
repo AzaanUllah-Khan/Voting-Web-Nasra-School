4c3f0fb46bb24eda34cf674dc31fdb3a65af7680
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-07-08 17:23:28 +0500

    images displayed too (Headboy.jsx) done

--- src/Pages/Headboy.jsx
+++ src/Pages/Headboy.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from '../Components/Navbar/Navbar';
 import Heading from '../Components/HeroHeading/Heading';
 import ImageComp from '../Components/ImageComp/Image';
@@ -6,37 +6,58 @@ import './css.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 import { useNavigate } from 'react-router-dom';
-import { query, getDocs, db,collection } from '../assets/Firebase/Firebase';
+import { query, getDocs, db, collection } from '../assets/Firebase/Firebase';
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 
 const Headboy = () => {
   const [isImageSelected, setIsImageSelected] = useState(false);
+  const [data, setData] = useState([]);
   const navigate = useNavigate();
 
   const handleImageSelect = (selected) => {
     setIsImageSelected(selected);
   };
 
-  function next() {
+  const next = () => {
     navigate('/headgirl');
-  }
-  const getData = async () => {
-    const q = query(collection(db, "Headboy"));
+  };
 
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach((doc) => {
-      console.log(doc.id, " => ", doc.data());
-    });
-  }
-  getData()
+  useEffect(() => {
+    const getData = async () => {
+      const q = query(collection(db, "Headboy"));
+      const querySnapshot = await getDocs(q);
+      const dataArray = [];
+      const storage = getStorage();
+
+      const promises = querySnapshot.docs.map(async (doc) => {
+        const name = doc.data().Name;
+        const imageRef = ref(storage, `Headboy/${name}`);
+        const img = await getDownloadURL(imageRef);
+        return {
+          name,
+          img,
+        };
+      });
+
+      const results = await Promise.all(promises);
+      setData(results);
+    };
+    getData();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <Heading post="Head Boy" />
       <div className='row'>
-        <ImageComp img="https://i0.wp.com/rollercoasteryears.com/wp-content/uploads/Thrive-During-Finals-.jpg?fit=1000%2C667&ssl=1" name="AzaanUllah Khan" onImageSelect={handleImageSelect} />
-        <ImageComp img="https://i0.wp.com/rollercoasteryears.com/wp-content/uploads/Thrive-During-Finals-.jpg?fit=1000%2C667&ssl=1" name="AzaanUllah Khan" onImageSelect={handleImageSelect} />
-        <ImageComp img="https://i0.wp.com/rollercoasteryears.com/wp-content/uploads/Thrive-During-Finals-.jpg?fit=1000%2C667&ssl=1" name="AzaanUllah Khan" onImageSelect={handleImageSelect} />
-        <ImageComp img="https://i0.wp.com/rollercoasteryears.com/wp-content/uploads/Thrive-During-Finals-.jpg?fit=1000%2C667&ssl=1" name="AzaanUllah Khan" onImageSelect={handleImageSelect} />
+        {data.map((item, index) => (
+          <ImageComp
+            key={index}
+            img={item.img}
+            name={item.name}
+            onImageSelect={handleImageSelect}
+          />
+        ))}
       </div>
       <button className='vote' onClick={next} disabled={!isImageSelected}>
         Confirm Your Vote <FontAwesomeIcon icon={faArrowRight} />
