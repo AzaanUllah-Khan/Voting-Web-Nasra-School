3f0bd97c8e23c971929083d8535cb2ec282a967a
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-07-19 19:18:24 +0500

    headgirl component setteled

--- src/Pages/Headgirl.jsx
+++ src/Pages/Headgirl.jsx
@@ -12,6 +12,8 @@ import { getStorage, ref, getDownloadURL } from "firebase/storage";
 const Headgirl = () => {
   const [selectedImageIndex, setSelectedImageIndex] = useState(null);
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   const navigate = useNavigate();
 
   const handleImageSelect = (index) => {
@@ -33,7 +35,6 @@ const Headgirl = () => {
     const getData = async () => {
       const q = query(collection(db, "Headgirl"));
       const querySnapshot = await getDocs(q);
-      const dataArray = [];
       const storage = getStorage();
 
       const promises = querySnapshot.docs.map(async (doc) => {
@@ -52,6 +53,7 @@ const Headgirl = () => {
 
       const results = await Promise.all(promises);
       setData(results);
+      setLoading(false);
     };
     getData();
   }, []);
@@ -61,16 +63,21 @@ const Headgirl = () => {
       <Navbar />
       <Heading post="Head Girl" />
       <div className='row'>
-        {data.map((item, index) => (
-          <ImageComp
-            key={index}
-            img={item.img}
-            name={item.name}
-            symbol={item.symbol}
-            isSelected={index === selectedImageIndex}
-            onClick={() => handleImageSelect(index)}
-          />
-        ))}
+        {loading
+          ? Array.from({ length: 4 }).map((_, index) => (
+              <ImageComp key={index} isLoading={true} />
+            ))
+          : data.map((item, index) => (
+              <ImageComp
+                key={index}
+                img={item.img}
+                name={item.name}
+                symbol={item.symbol}
+                isSelected={index === selectedImageIndex}
+                onClick={() => handleImageSelect(index)}
+                isLoading={false}
+              />
+            ))}
       </div>
       <button className='vote' onClick={next} disabled={selectedImageIndex === null}>
         Confirm Your Vote <FontAwesomeIcon icon={faArrowRight} />
