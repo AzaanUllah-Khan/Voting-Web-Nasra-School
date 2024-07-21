dc3d27561cae5f440e7d547a0af88b7c323429de
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-07-22 01:07:22 +0500

    finish page made with antd

--- src/Pages/Finish.jsx
+++ src/Pages/Finish.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Result,Button } from "antd";
+import { SmileOutlined } from '@ant-design/icons';
+import { useNavigate } from 'react-router-dom';
 
 const Finish = () => {
+  var navv = useNavigate()
+  function nav(){
+    navv('/')
+  }
   return (
-    <div>
-      
+    <div style={{height:"100vh",width:"100%",display:"flex",alignItems:"center",justifyContent:"center"}}>
+    <Result
+    icon={<SmileOutlined />}
+    title="Great, we have done all the operations!"
+    extra={<Button type="primary" onClick={nav}>Next</Button>}/>
     </div>
   )
 }
