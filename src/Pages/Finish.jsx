f9008a81fd829501d16e2bdd3ce593cfdd737d0a
AzaanUllah-Khan <azaanullahkhan291@gmail.com>
2024-07-27 22:00:53 +0500

    credits added

--- src/Pages/Finish.jsx
+++ src/Pages/Finish.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import { Result,Button } from "antd";
+import { Result, Button } from "antd";
 import { SmileOutlined } from '@ant-design/icons';
 import { useNavigate } from 'react-router-dom';
 
 const Finish = () => {
   var navv = useNavigate()
-  function nav(){
+  function nav() {
     navv('/')
   }
   return (
-    <div style={{height:"100vh",width:"100%",display:"flex",alignItems:"center",justifyContent:"center"}}>
-    <Result
-    icon={<SmileOutlined />}
-    title="Great, we have done all the operations!"
-    extra={<Button type="primary" onClick={nav}>DONE</Button>}/>
+    <div style={{ height: "100vh", width: "100%", display: "flex", alignItems: "center", justifyContent: "center" }}>
+      <Result
+        icon={<SmileOutlined />}
+        extra={<div style={{display:"flex",flexDirection:"column",alignItems:"center",gap:"10px"}}>
+          <h1 style={{color:"#aaa"}}>Great, we have done all the operations!</h1>
+          <Button type="primary" onClick={nav}>DONE</Button>
+          <p style={{color:"#ccc"}}>Developed By AzaanUllah Khan</p>
+        </div>
+        } />
     </div>
   )
 }
